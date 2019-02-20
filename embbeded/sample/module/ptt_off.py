e437924e3f2ed9809d7aed179816704bbe4d3162
Bernino Falya <falya.bernino@gmail.com>
2019-02-21 02:58:18 +0700

    new changes in shell script

--- embbeded/sample/module/ptt_off.py
+++
@@ -1,5 +0,0 @@
-import RPi.GPIO as GPIO 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-GPIO.output(12, GPIO.LOW)
-GPIO.cleanup()
--- embbeded/sample/shell/send_sstv.sh
+++ embbeded/sample/shell/send_sstv.sh
@@ -1,10 +1,23 @@
-echo "1" > /sys/class/gpio/gpio18/value
+if [[ -n $PRODUCTION ]]; then 
+    echo "1" > /sys/class/gpio/gpio18/value
+fi
+
 play ./aprs_reply/process_convert_sstv.wav 
-echo "0" > /sys/class/gpio/gpio18/value
+
+if [[ -n $PRODUCTION ]]; then 
+    echo "0" > /sys/class/gpio/gpio18/value
+fi 
+
 python -m pysstv ./capture/$1 output.wav --mode $2
 
-echo "1" > /sys/class/gpio/gpio18/value
+if [[ -n $PRODUCTION ]]; then 
+    echo "1" > /sys/class/gpio/gpio18/value
+fi
+
 play output.wav
-echo "0" > /sys/class/gpio/gpio18/value
+
+if [[ -n $PRODUCTION ]]; then 
+    echo "0" > /sys/class/gpio/gpio18/value
+fi
 
 

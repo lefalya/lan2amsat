501892699d51b0db970a4c9e1975bcc32e4f015e
Bernino Falya <falya.bernino@gmail.com>
2019-03-02 07:44:36 +0700

    gitignore fix

--- embbeded/sample/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source "../bin/activate"
-
-if [[ -n $PRODUCTION ]]; then 
-    echo "18" > /sys/class/gpio/export 
-    echo "out" > /sys/class/gpio/direction 
-fi
-direwolf &
-python main.py

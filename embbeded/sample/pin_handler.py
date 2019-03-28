72557a28075934c85f8161aba9709f4ad6ac00b1
Bernino Falya <falya.bernino@gmail.com>
2019-03-28 13:12:07 +0700

    added debounce interval calc

--- embbeded/sample/pin_handler.py
+++ embbeded/sample/pin_handler.py
@@ -1,4 +1,7 @@
 import RPi.GPIO as GPIO 
+import datetime 
+from collections import deque 
+
 
 D_TX = 8
 D_RX = 10 
@@ -9,15 +12,24 @@ CAMERA_CAPTURE = 12
 
 class pinHandler : 
 
-    def __init__(): 
+    def __init__(self): 
         GPIO.setup([P1, P2, CAMERA_CAPTURE], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
         GPIO.setup([AOS_SAT], GPIO.OUT, initial=GPIO.HIGH)
         GPIO.add_event_detect(P1, GPIO.BOTH, self.handle) 
         GPIO.add_event_detect(P2, GPIO.BOTH, self.handle)         
         GPIO.add_event_detect(CAMERA_CAPTURE, GPIO.RISING, self.handle) 
-    
-    def handle(pin): 
-        print(pin)
 
-    def aos_out(): 
+        #interval queue
+        self.qint = deque(maxlen=2)
+
+    
+    def handle(self, pin): 
+        dt = datetime.utcnow() 
+        microsecond = dt.microsecond 
+        self.qint.append(microsecond) 
+        if(len(self.qint) ==2):
+            interval = self.qint[1]-self.qint[0] 
+            print(pin, interval)
+
+    def aos_out(self): 
         GPIO.output(AOS_SAT, GPIO.HIGH) 

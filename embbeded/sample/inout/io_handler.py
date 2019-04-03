fdca16b7c2ac95e48041698bcb76fe22c0b94031
Bernino Falya <falya.bernino@gmail.com>
2019-04-03 13:13:59 +0700

    new folder structure, added fifo

---
+++ embbeded/sample/inout/io_handler.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO 
+import time
+from datetime import datetime 
+from collections import deque 
+
+D_TX = 8
+D_RX = 10 
+P1 = 11
+P2 = 13
+AOS_SAT = 15
+CAMERA_CAPTURE = 12 
+
+class io_handler : 
+
+    def __init__(self): 
+        #interval queue
+        self.qint = deque(maxlen=2)
+        self.camera_thold = false 
+        self.camera_int_time = '' 
+    
+    def handle(self, pin):
+        dt = datetime.utcnow() 
+        self.qint.append(dt)  
+        if(len(self.qint) ==2):
+            interval = (self.qint[1]-self.qint[0]).total_seconds()
+            print(self.qint[1]," - ",self.qint[0]," = ",interval)
+
+    def aos_out(self): 
+        GPIO.output(AOS_SAT, GPIO.HIGH) 
+'''
+if __name__ == "__main__" : 
+    io = io_handler()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup([P1, P2, CAMERA_CAPTURE], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+    GPIO.setup([AOS_SAT], GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.add_event_detect(P1, GPIO.FALLING, ph.handle) 
+    GPIO.add_event_detect(P2, GPIO.BOTH, ph.handle)         
+    GPIO.add_event_detect(CAMERA_CAPTURE, GPIO.RISING, ph.handle) 
+
+    while True : 
+        time.sleep(1e6) 
+'''

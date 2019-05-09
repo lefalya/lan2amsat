69247b28b4e7fab7a97c6da96f899981edc0d1cd
Bernino Falya <falya.bernino@gmail.com>
2019-05-09 13:01:30 +0700

    added PTT handler

--- embbeded/sample/inout/io_handler.py
+++ embbeded/sample/inout/io_handler.py
@@ -6,25 +6,26 @@ from inout import camera_handler
 import RPi.GPIO as GPIO 
 import time
 
-D_TX = 8
-D_RX = 10 
-P1 = 11
-P2 = 13
-AOS_SAT = 15
-CAMERA_CAPTURE = 12 
-
 class io_handler : 
 
     def __init__(self, **kwargs): 
 
         # Injected instance, apply rule [2]
         self.master_fifo = ''
+
+        self.D_TX = 8
+        self.D_RX = 10 
+        self.P1 = 11
+        self.P2 = 13
+        self.AOS_SAT = 15
+        self.CAMERA_CAPTURE = 12 
+        self.PTT = 7
         
         self.datetime = kwargs['datetime']
 
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup([P1, P2, CAMERA_CAPTURE], GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-        #GPIO.setup([AOS_SAT], GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setup([self.P1, self.P2, self.CAMERA_CAPTURE], GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+        GPIO.setup([[self.PTT], GPIO.OUT, initial=GPIO.LOW)
         GPIO.add_event_detect(P1, GPIO.FALLING, self.handle) 
         GPIO.add_event_detect(P2, GPIO.BOTH, self.handle)         
         GPIO.add_event_detect(CAMERA_CAPTURE, GPIO.FALLING, self.handle) 
@@ -44,6 +45,12 @@ class io_handler :
     def handle(self, pin):
         if(pin == CAMERA_CAPTURE): 
             self.camera_handler.trigger()             
+    
+    def ptt_high(self):
+        GPIO.output(self.PTT, GPIO.HIGH)
+
+    def ptt_low(self):
+        GPIO.output(self.PTT, GPIO.LOW)    
 
     def aos_out(self): 
-        GPIO.output(AOS_SAT, GPIO.HIGH) 
+        GPIO.output(self.AOS_SAT, GPIO.HIGH) 
